ff3a06fd5c7e466ea092782452ab38a77fb5674b
Mike Rodrigues <rodrigues.mi@husky.neu.edu>
2018-11-18 19:21:09 -0500

    Updating structure, dependencies, tests

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # A Document Path Library for Node
 
-[![Build Status](https://travis-ci.org/mrodrig/doc-path.svg?branch=master)](https://travis-ci.org/mrodrig/doc-path)
 [![Dependencies](https://img.shields.io/david/mrodrig/doc-path.svg?style=flat-square)](https://www.npmjs.org/package/doc-path)
+[![Build Status](https://travis-ci.org/mrodrig/doc-path.svg?branch=master)](https://travis-ci.org/mrodrig/doc-path)
 [![Monthly Downloads](http://img.shields.io/npm/dm/doc-path.svg)](https://www.npmjs.org/package/doc-path)
 [![NPM version](https://img.shields.io/npm/v/doc-path.svg)](https://www.npmjs.org/package/doc-path)
+[![Maintainability](https://api.codeclimate.com/v1/badges/8d357f67aa5aaf8d727e/maintainability)](https://codeclimate.com/github/mrodrig/doc-path/maintainability)
 [![Known Vulnerabilities](https://snyk.io/test/npm/doc-path/badge.svg)](https://snyk.io/test/npm/doc-path)
 
 This module will take paths in documents which can include nested paths specified by '.'s and can evaluate the path
@@ -18,7 +19,7 @@ $ npm install doc-path
 ## Usage
 
 ```javascript
-var path = require('doc-path');
+let path = require('doc-path');
 ```
 
 ### API
@@ -33,9 +34,9 @@ If the key does not exist, `undefined` is returned.
 ##### path.evaluatePath Example:
 
 ```javascript
-var path = require('doc-path');
+const path = require('doc-path');
 
-var document = {
+let document = {
     Make: 'Nissan',
     Model: 'Murano',
     Year: '2013',
@@ -64,9 +65,9 @@ If no document is provided, an error will be thrown.
 #### path.setPath Example:
 
  ```javascript
- var path = require('doc-path');
+ const path = require('doc-path');
 
- var document = {
+ let document = {
      Make: 'Nissan'
  };
 
@@ -92,10 +93,10 @@ $ npm run coverage
 
 Current Coverage is:
 ```
-Statements   : 100% ( 22/22 )
-Branches     : 90% ( 9/10 )
+Statements   : 100% ( 21/21 )
+Branches     : 100% ( 12/12 )
 Functions    : 100% ( 2/2 )
-Lines        : 100% ( 19/19 )
+Lines        : 100% ( 18/18 )
 ```
 
 ## Features
--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,113 @@
-var pathTests     = require('./testPath');
+let path = require('../src/path'),
+    should = require('should'),
+    assert = require('assert'),
+    _ = require('underscore'),
+    doc = {};
 
 describe('doc-path Module', function() {
-    pathTests.runTests();
+    describe('evaluatePath', function () {
+        beforeEach(function () {
+            doc = {};
+        });
+
+        it('should get a non-nested property that exists', function(done) {
+            doc.testProperty = 'testValue';
+            let returnVal = path.evaluatePath(doc, 'testProperty');
+            returnVal.should.equal('testValue');
+            done();
+        });
+
+        it('should return null if the non-nested property does not exist', function(done) {
+            let returnVal = path.evaluatePath(doc, 'testProperty');
+            assert.equal(returnVal, null);
+            done();
+        });
+
+        it('should get a non-nested property that exists', function(done) {
+            doc.testProperty = {testProperty2:'testValue'};
+            let returnVal = path.evaluatePath(doc, 'testProperty.testProperty2');
+            returnVal.should.equal('testValue');
+            done();
+        });
+
+        it('should return null if the nested property does not exist', function(done) {
+            let returnVal = path.evaluatePath(doc, 'testProperty.testProperty2');
+            assert.equal(returnVal, null);
+            done();
+        });
+
+        it('should work with multiple accesses', function (done) {
+            doc = {
+                testProperty : {
+                    testProperty2: 'testVal'
+                },
+                'testProperty3' : 'testVal2'
+            };
+            let returnVal = path.evaluatePath(doc, 'testProperty.testProperty2');
+            assert.equal(returnVal, 'testVal');
+            returnVal = path.evaluatePath(doc, 'testProperty3');
+            assert.equal(returnVal, 'testVal2');
+            done();
+        });
+
+        it('should prioritize work with equal key value', function (done) {
+            doc = {
+                testProperty : {
+                    testProperty2: 'testVal'
+                },
+                'testProperty.testProperty2' : 'testVal2'
+            };
+            let returnVal = path.evaluatePath(doc, 'testProperty.testProperty2');
+            assert.equal(returnVal, 'testVal2');
+            done();
+        });
+    });
+
+    describe('setPath', function () {
+        beforeEach(function () {
+            doc = {};
+        });
+
+        it('should get a non-nested property that exists', function(done) {
+            let returnVal = path.setPath(doc, 'testProperty', 'null');
+            assert.equal(returnVal, doc);
+            done();
+        });
+
+        it('should return null if the non-nested property does not exist', function(done) {
+            try {
+                doc = null;
+                assert.equal(doc, null);
+                let returnVal = path.setPath(doc, 'testProperty', 'null');
+            } catch (err) {
+                err.message.should.equal('No document was provided.');
+                done();
+            }
+        });
+
+        it('should get a non-nested property that exists', function(done) {
+            let returnVal = path.setPath(doc, 'testProperty.testProperty2', 'testValue');
+            assert.equal(returnVal, doc);
+            done();
+        });
+
+        it('should return null if the nested property does not exist', function(done) {
+            try {
+                doc = null;
+                assert.equal(doc, null);
+                let returnVal = path.setPath(doc, 'testProperty.test', 'null');
+            } catch (err) {
+                err.message.should.equal('No document was provided.');
+                done();
+            }
+        });
+
+        it('should work with multiple accesses', function (done) {
+            let returnVal = path.setPath(doc, 'testProperty.testProperty2', 'testVal');
+            assert.equal(returnVal, doc);
+            returnVal = path.setPath(doc, 'testProperty.testProperty2', 'testVal2');
+            assert.equal(returnVal, doc);
+            done();
+        });
+    });
 });
